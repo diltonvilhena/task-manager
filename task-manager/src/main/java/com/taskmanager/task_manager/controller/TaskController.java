c927f0a7fb73a843b4a0a76a877e9953ae7cf8e3
19/05 1.1 <diltonvilhena@gmail.com>
2025-03-18 08:31:35 -0300

    feat: criação Controller

---
+++ task-manager/src/main/java/com/taskmanager/task_manager/controller/TaskController.java
@@ -0,0 +1,28 @@
+package com.taskmanager.task_manager.controller;
+
+import com.taskmanager.task_manager.model.Task;
+import com.taskmanager.task_manager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+}
