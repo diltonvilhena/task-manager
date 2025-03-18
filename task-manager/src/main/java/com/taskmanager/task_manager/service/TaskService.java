22043c732f18840281231fdb63ff9212058bd95f
19/05 1.1 <diltonvilhena@gmail.com>
2025-03-18 08:31:04 -0300

    feat: criação Service

---
+++ task-manager/src/main/java/com/taskmanager/task_manager/service/TaskService.java
@@ -0,0 +1,25 @@
+package com.taskmanager.task_manager.service;
+
+import com.taskmanager.task_manager.model.Task;
+import com.taskmanager.task_manager.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+}
