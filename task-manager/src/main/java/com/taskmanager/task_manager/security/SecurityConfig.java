19a48204bbdc686c0fbcc40c1d993e1faed3da5d
19/05 1.1 <diltonvilhena@gmail.com>
2025-03-27 22:23:33 -0300

    feat: criação SecurityConfig

---
+++ task-manager/src/main/java/com/taskmanager/task_manager/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.taskmanager.task_manager.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Define que a API não usa sessões (Stateless)
+
+        return http.build();
+    }
+}
+
+
